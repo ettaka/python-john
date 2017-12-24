d8d583c6a151fb0a534204d52be9cef28eb53cc1
eelis <eelis@gmail.com>
2017-12-24 12:59:42 +0200

    Add joohb.py

---
+++ joohb.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+print "gggjohmm"
+name = raw_input("What is your name?\n")
+if name == "John":
+  print name, "is the best!"
+else:
+  print name, "est fou!"
+
+
+age = raw_input("Kuinka vanha olet?\n")
+
+if age == "6":
+  print "your age is the best"
+else:
+  print "tu es fou!"
+
